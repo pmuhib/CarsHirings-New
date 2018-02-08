4aae6ed367a49b4b064f3e534837bec95a8a5667
Muhib <muhib@askonlinesolutions.com>
2018-02-08 12:03:00 +0530

    Added by Muhib

---
+++ app/src/main/java/com/carshiring/webservices/ApiResponse.java
@@ -0,0 +1,18 @@
+package com.carshiring.webservices;
+
+/**
+ * Created by Muhib.
+ * Contact Number : +91 9796173066
+ */
+class ApiResponse {
+    public int error_code ;
+    public boolean status ;
+    public String msg ;
+    public String access_token ;
+    public long expires_in ;
+    public String token_type ;
+    public String scope ;
+    public String error ;
+    public String error_description ;
+    public Data response ;
+}
---
+++ app/src/main/java/com/carshiring/webservices/RetrofitApiBuilder.java
@@ -0,0 +1,67 @@
+package com.carshiring.webservices;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Muhib.
+ * Contact Number : +91 9796173066
+ */
+public class RetrofitApiBuilder {
+    public final static String CarHires_BASE_URL = "http://webdevelopmentreviews.net/carhires/webservices/";
+    public final static String CarGates_BASE_URL = "https://carsgates.com/";
+    private final static String CarGates_BASE_WEBSERVICE_URL = CarGates_BASE_URL  + "webservices/";
+    public final static String IMG_BASE_URL = CarHires_BASE_URL+ "upload/";
+
+    public static RetroFitApis getCargHiresapis()
+    {
+        OkHttpClient httpClient=new OkHttpClient.Builder()
+                .connectTimeout(10000, TimeUnit.SECONDS)
+                .writeTimeout(10000,TimeUnit.SECONDS)
+                .readTimeout(30000,TimeUnit.SECONDS)
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request request;
+                        request=chain.request().newBuilder().addHeader("content-type","application/x-www-form-urlencoded").build();
+                        return chain.proceed(request);
+                    }
+                })
+                .build();
+        Retrofit retrofit=new Retrofit.Builder().baseUrl(CarHires_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient)
+                .build();
+        return retrofit.create(RetroFitApis.class);
+    }
+    public static  RetroFitApis getCarGatesapi()
+    {
+        OkHttpClient client =new OkHttpClient.Builder()
+                .writeTimeout(10000,TimeUnit.SECONDS)
+                .readTimeout(10000,TimeUnit.SECONDS)
+                .connectTimeout(10000,TimeUnit.SECONDS)
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request request;
+                        request=chain.request()
+                                .newBuilder()
+                                .addHeader("content-type","application/x-www-form-urlencoded")
+                                .build();
+                        return chain.proceed(request);
+                    }
+                }).build();
+        Retrofit retrofit=new Retrofit.Builder().baseUrl(CarGates_BASE_WEBSERVICE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+        return retrofit.create(RetroFitApis.class);
+    }
+}
