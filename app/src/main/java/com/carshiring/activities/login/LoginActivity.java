c217d2f7840de6272d0a5f2687d18b6720447eef
lenovo <rakhi@askonlinesolution.com>
2018-02-08 12:24:21 +0530

    Change by rakhi 8/2/2018 12:24PM

--- app/src/main/java/com/carshiring/activities/login/LoginActivity.java
+++ app/src/main/java/com/carshiring/activities/login/LoginActivity.java
@@ -13,6 +13,7 @@ public class LoginActivity extends AppBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 
     public void skipLogin(View view) {
